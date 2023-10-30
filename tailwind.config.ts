086d909234b8b4717629b907a102ee59267a61af
Quang Trinh <quang.tk@carehealth.io>
2023-10-30 11:13:52 +0700

    Remove unused

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -51,11 +51,6 @@ const config: Config = {
         box: "0 1px 10px 0 rgba(0, 0, 0, 0.20)",
         "box-md": "0 1px 10px 2px rgba(0, 0, 0, 0.30)",
       },
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
       keyframes: {
         "hero-height": {
           "0%, 50%": {
