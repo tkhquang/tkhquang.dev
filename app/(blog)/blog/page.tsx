8220eb5857dfef357cd7d13533940cbc85871c45
Quang Trinh <khacquang.trinh@gmail.com>
2025-05-30 02:25:33 +0700

    fix: wrong info path

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -6,5 +6,7 @@ export const revalidate = 86400;
 export default async function BlogPage() {
   const posts = await _MarkdownParser.getAllPosts();
 
-  return <NewsFeed posts={posts} title="" pathSlug="categories" />;
+  return (
+    <NewsFeed posts={posts} pathSlug="categories" pathInfoType="category" />
+  );
 }
