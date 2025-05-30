c833a1b28e1920f631530e2761731961e613b149
Quang Trinh <khacquang.trinh@gmail.com>
2025-05-30 11:46:58 +0700

    chore: ignore some paths in matcher

--- middleware.ts
+++ middleware.ts
@@ -4,3 +4,39 @@ import type { NextRequest } from "next/server";
 export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     */
+    {
+      missing: [
+        { key: "next-router-prefetch", type: "header" },
+        { key: "purpose", type: "header", value: "prefetch" },
+      ],
+      source:
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    },
+
+    {
+      has: [
+        { key: "next-router-prefetch", type: "header" },
+        { key: "purpose", type: "header", value: "prefetch" },
+      ],
+      source:
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    },
+
+    {
+      has: [{ key: "x-present", type: "header" }],
+      missing: [{ key: "x-missing", type: "header", value: "prefetch" }],
+      source:
+        "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+    },
+  ],
+};
