9094e37f7ce25a8225bf42c8b5b74b710017ba81
Quang Trinh <khacquang.trinh@gmail.com>
2025-06-02 23:20:44 +0700

    build: try fixing remote image

--- src/lib/rehype-custom-next-image/index.ts
+++ src/lib/rehype-custom-next-image/index.ts
@@ -99,7 +99,7 @@ export default function rehypeCustomNextImage(
             blurDataURL: result?.metadata.dataURIBase64,
             height: height ?? 720,
             placeholder: "blur",
-            src: filePath.includes(targetPath)
+            src: filePath.includes(targetPath.replace(/^\.\//, ""))
               ? `${process.env.NEXT_PUBLIC_BASE_URL}/${finalSrc}`
               : finalSrc,
             width: width ?? 1280,
