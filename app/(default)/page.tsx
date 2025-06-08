8986c05009db5148bd129c09fe0ca4a2bb9c02db
Quang Trinh <khacquang.trinh@gmail.com>
2025-06-09 04:15:56 +0700

    feat: track home page view count

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -1,3 +1,5 @@
+import { Suspense } from "react";
+import ReportView from "@/components/common/ReportView";
 import Contact from "@/components/contact/Contact";
 import Landing from "@/components/landing/Landing";
 import Stacks from "@/components/landing/stacks/Stacks";
@@ -9,6 +11,9 @@ export const revalidate = 86400;
 export default async function HomePage() {
   return (
     <>
+      <Suspense>
+        <ReportView />
+      </Suspense>
       <Landing />
       <Stacks />
       <Projects />
