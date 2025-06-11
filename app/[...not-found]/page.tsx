e5f5369efd9746684619d4ebcb44505b89cafaca
Quang Trinh <khacquang.trinh@gmail.com>
2025-06-11 16:54:46 +0700

    chore: fix image size

--- app/[...not-found]/page.tsx
+++ app/[...not-found]/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 function NotFoundPage() {
   return (
     <div className="typography container mt-header-height flex flex-1 flex-col">
-      <div className="my-8 flex flex-1 flex-col">
+      <div className="my-4 flex flex-1 flex-col md:my-8">
         <h1 className="">Oops! We have looked everywhere...</h1>
         <p className="">
           But we couldn&apos;t find what you are looking for.
@@ -13,7 +13,7 @@ function NotFoundPage() {
           <Link href="/blog/categories">post archive</Link> is full of hidden
           gems. Maybe your missing post is just playing hide-and-seek!
         </p>
-        <div className="relative flex min-h-[300px] flex-1 flex-col items-center justify-center">
+        <div className="relative flex min-h-[200px] flex-1 flex-col items-center justify-center">
           <Image src="/assets/resources/images/404-jim.gif" alt="404" fill />
         </div>
       </div>
