0541beccee1ee95e3bbe0a95563c92c651be5878
Quang Trinh <quang.trinh@truckerpath.com>
2025-11-04 13:47:00 +0700

    chore: update build settings

--- next.config.mjs
+++ next.config.mjs
@@ -58,6 +58,7 @@ const nextConfig = {
     },
   },
   outputFileTracingIncludes: {
+    "/*": ["node_modules/sharp/**/*"],
     "/blog{,/**/*}": ["./content/**"],
     "/api/pdf{,/**/*}": [
       "node_modules/@sparticuz/chromium/**/*",
